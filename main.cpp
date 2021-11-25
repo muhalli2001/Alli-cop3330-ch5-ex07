881efb2b32d31b39fae6200c54ebf3b7941587a6
muhalli2001 <muhalli2001@knights.ucf.edu>
2021-11-24 21:01:46 -0500

    commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Muhammad Alli
+ * 
+ * */
+
 #include <iostream>
 #include "std_lib_facilities.h"
 
-//
+
 double num_roots(double a, double b, double c)
 {
     b = b*b;
